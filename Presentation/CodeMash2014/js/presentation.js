04a226e5b6c5c35b6bad03e7b94668850b1a345d
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2013-11-26 22:23:46 -0500

    Added links slide (needs work), added angular script file and module, moved files

---
+++ Presentation/CodeMash2014/js/presentation.js
@@ -0,0 +1,7 @@
+(function () {
+	var codemash2014 = angular.module('codemash2014',[]);
+
+	codemash2014.controller('bindingDemoCtrl', ['$scope', function ($scope) {
+		$scope.textStuff = 'stuff';
+	}]);
+})();
