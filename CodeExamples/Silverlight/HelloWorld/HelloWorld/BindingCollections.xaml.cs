390bdc4edee84e67b0dbc89197013147850dc1dd
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2013-12-03 21:49:47 -0500

    Added more binding info and collection binding start

---
+++ CodeExamples/Silverlight/HelloWorld/HelloWorld/BindingCollections.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+	public partial class BindingCollections : UserControl
+	{
+		public BindingCollections()
+		{
+			InitializeComponent();
+
+			List.ItemsSource = new[] { "Jim", "John", "Bob" };
+
+			var cvs = new CollectionViewSource();
+			cvs.Source = new[] { "Jim", "John", "Bob" };
+
+			List2.ItemsSource = cvs.View;
+		}
+	}
+}
--- Presentation/CodeMash2014/js/presentation.js
+++ Presentation/CodeMash2014/js/presentation.js
@@ -1,7 +1,11 @@
 (function () {
-	var codemash2014 = angular.module('codemash2014',[]);
+	var codemash2014 = angular.module('codemash2014', []);
 
 	codemash2014.controller('bindingDemoCtrl', ['$scope', function ($scope) {
-		$scope.textStuff = 'stuff';
+		$scope.textStuff = 'World';
+	}]);
+
+	codemash2014.controller('collectionBindingCtrl', ['$scope', function ($scope) {
+		$scope.names = ['Jim', 'John', 'Bob'];
 	}]);
 })();
