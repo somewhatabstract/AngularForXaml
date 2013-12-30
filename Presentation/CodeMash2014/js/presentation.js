10ed3082465c8b149c568d610948bf1ad8a5f1c0
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2013-12-30 17:45:15 -0500

    Implemented generator in angular and tweaked Silverlight styles
    Update angular to 1.2.6

--- Presentation/CodeMash2014/js/presentation.js
+++ Presentation/CodeMash2014/js/presentation.js
@@ -56,4 +56,215 @@
 			return statusColorService.getTextColor(hasError);
 		};
 	}]);
+
+	codemash2014.value('insultWords', {
+		firstAdjectives: [
+			"artless",
+			"bawdy",
+			"beslubbering",
+			"bootless",
+			"churlish",
+			"cockered",
+			"clouted",
+			"craven",
+			"currish",
+			"dankish",
+			"dissembling",
+			"droning",
+			"errant",
+			"fawning",
+			"fobbing",
+			"froward",
+			"frothy",
+			"gleeking",
+			"goatish",
+			"gorbellied",
+			"impertinent",
+			"infectious",
+			"jarring",
+			"loggerheaded",
+			"lumpish",
+			"mammering",
+			"mangled",
+			"mewling",
+			"paunchy",
+			"pribbling",
+			"puking",
+			"puny",
+			"qualling",
+			"rank",
+			"reeky",
+			"roguish",
+			"ruttish",
+			"saucy",
+			"spleeny",
+			"spongy",
+			"surly",
+			"tottering",
+			"unmuzzled",
+			"vain",
+			"venomed",
+			"villainous",
+			"warped",
+			"wayward",
+			"weedy",
+			"yeasty"],
+		secondAdjectives: [
+			"base-court",
+			"bat-fowling",
+			"beef-witted",
+			"beetle-headed",
+			"boil-brained",
+			"clapper-clawed",
+			"clay-brained",
+			"common-kissing",
+			"crook-pated",
+			"dismal-dreaming",
+			"dizzy-eyed",
+			"dog-hearted",
+			"dread-bolted",
+			"earth-vexing",
+			"elf-skinned",
+			"fat-kidneyed",
+			"fen-sucked",
+			"flap-mouthed",
+			"fly-bitten",
+			"folly-fallen",
+			"fool-born",
+			"full-gorged",
+			"guts-griping",
+			"half-faced",
+			"hasty-witted",
+			"hedge-born",
+			"hell-hated",
+			"idle-headed",
+			"ill-breeding",
+			"ill-nurtured",
+			"knotty-pated",
+			"milk-livered",
+			"motley-minded",
+			"onion-eyed",
+			"plume-plucked",
+			"pottle-deep",
+			"pox-marked",
+			"reeling-ripe",
+			"rough-hewn",
+			"rude-growing",
+			"rump-fed",
+			"shard-borne",
+			"sheep-biting",
+			"spur-galled",
+			"swag-bellied",
+			"tardy-gaited",
+			"tickle-brained",
+			"toad-spotted",
+			"unchin-snouted",
+			"weather-bitten"],
+		nouns: [
+			"apple-john",
+			"baggage",
+			"barnacle",
+			"bladder",
+			"boar-pig",
+			"bugbear",
+			"bum-bailey",
+			"canker-blossom",
+			"clack-dish",
+			"clotpole",
+			"coxcomb",
+			"codpiece",
+			"death-token",
+			"dewberry",
+			"flap-dragon",
+			"flax-wench",
+			"flirt-gill",
+			"foot-licker",
+			"fustilarian",
+			"giglet",
+			"gudgeon",
+			"haggard",
+			"harpy",
+			"hedge-pig",
+			"horn-beast",
+			"hugger-mugger",
+			"joithead",
+			"lewdster",
+			"lout",
+			"maggot-pie",
+			"malt-worm",
+			"mammet",
+			"measle",
+			"minnow",
+			"miscreant",
+			"moldwarp",
+			"mumble-news",
+			"nut-hook",
+			"pigeon-egg",
+			"pignut",
+			"puttock",
+			"pumpion",
+			"ratsbane",
+			"scut",
+			"skainsmate",
+			"strumpet",
+			"varlot",
+			"vassal",
+			"whey-face",
+			"wagtail"]
+	});
+
+	codemash2014.controller('insultGeneratorCtrl', function($scope, insultWords) {
+		$scope.firstAdjectives = insultWords.firstAdjectives;
+		$scope.secondAdjectives = insultWords.secondAdjectives;
+		$scope.nouns = insultWords.nouns;
+
+		$scope.randomize = function() {
+			$scope.firstAdjective = getRandomElement($scope.firstAdjectives);
+			$scope.secondAdjective = getRandomElement($scope.secondAdjectives);
+			$scope.noun = getRandomElement($scope.nouns);
+		};
+
+		function getRandomIndex(limit) {
+			return Math.round(limit * Math.random() + 0.5);
+		}
+
+		function getRandomElement(array) {
+			var index = getRandomIndex(array.length - 1);
+			return array[index];
+		}
+
+		$scope.randomize();
+	});
+
+	codemash2014.directive('wordList', function() {
+		return {
+			template: '<div><div class="label">{{title}}</div><select ng-model="selectedWord" ng-options="word for word in words"></select></div>',
+			restrict: 'E',
+			replace: true,
+			scope: {
+				selectedWord: '=',
+				words: '=',
+				title: '@'
+			}
+		};
+	});
+
+	codemash2014.directive('insultGenerator', function() {
+		return {
+			controller: 'insultGeneratorCtrl',
+			replace:true,
+			template:
+				'<div class="insult-generator">'
+				+ '<word-list title="Adjective 1:" words="firstAdjectives" selected-word="firstAdjective"></word-list>'
+				+ '<word-list title="Adjective 2:" words="secondAdjectives" selected-word="secondAdjective"></word-list>'
+				+ '<word-list title="Noun:" words="nouns" selected-word="noun"></word-list>'
+				+ '<div>'
+					+ '<button ng-click="randomize()">Randomize</button>'
+					+ '<div class="insult-container"><textarea class="insult" readonly rows="2">Thou {{firstAdjective}} {{secondAdjective}} {{noun}}!</textarea></div>'
+				+ '</div>'
+			  + '</div>',
+			restrict: 'E',
+			scope: {}
+		};
+	});
 })();
